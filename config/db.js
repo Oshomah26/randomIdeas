35fa420cf7c6d0ae39510a7402e92ea393c0aaef
Oshomah <oshomah26@gmail.com>
2023-06-21 16:53:49 +0100

    Was able to establish contact with the Mongo DB server and also install compass locally

--- config/db.js
+++ config/db.js
@@ -5,6 +5,6 @@ const connectDB = async () => {
     console.log(`MongoDB Connected: ${conn.connection.host}`);
 } 
 
-// mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', true);
 
 module.exports = connectDB;

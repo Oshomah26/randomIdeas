9afabace95b9dd87148a142c754a072c8380636c
Oshomah <oshomah26@gmail.com>
2023-07-02 17:23:00 +0100

    More code clean up

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import IdeaForm from './components/IdeaForm';
 import IdeaList from './components/IdeaList';
 import './css/style.css';
 
-
 new Modal();
 const ideaForm = new IdeaForm();
 ideaForm.render();
